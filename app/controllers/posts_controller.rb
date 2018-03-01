5651dc44971824e3d3e1c6d2e0ed1c121a5cc621
Jeff Dembinski <jeff@magicquotes.io>
2018-03-01 13:30:07 -0500

    Completes CRUD2 Checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,31 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all 
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+
+      flash[:notice] = "Post was saved"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving your post. Please try again."
+      render :new
+    end
   end
 end
