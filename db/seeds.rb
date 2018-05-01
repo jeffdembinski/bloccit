a04c66730a2060e084e6d941ff7b412d53da0ece
Jeff Dembinski <jeff@magicquotes.io>
2018-05-01 16:25:39 -0400

    Completes Bloccit Topics Assignment - SponsoredPosts

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,16 @@ end
 
 posts = Post.all
 
+50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+
+sponsored_posts = SponsoredPost.all
+
 # Create comments
 
 100.times do
@@ -31,6 +41,7 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
