c97fb256b9d18b6a485c0e2e04b132e576168aa3
Jeff Dembinski <jdembinski@gmail.com>
2018-05-09 20:37:35 -0400

    Completes Bloccit Signup Assignment - 2nd Try

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
+    helper_method :user_params
 
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    @user = User.new(user_params)
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
@@ -20,4 +17,16 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def confirm
+    @user = User.new(user_params)
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
+  end
+
+
 end
