dcd8687caba53fe9fcf05409206d72acf2d3c386
Jeff Dembinski <jdembinski@gmail.com>
2018-05-11 11:42:50 -0400

    Completes Bloccit Favorites Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+
   belongs_to :topic
   belongs_to :user
   has_many :comments, dependent: :destroy
@@ -11,6 +12,8 @@ class Post < ApplicationRecord
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  after_create :favorite_post
 
   def up_votes
     votes.where(value: 1).count
@@ -28,6 +31,13 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def favorite_post
+    Favorite.create!(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 
 end
