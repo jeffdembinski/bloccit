c242f7913f1b1330b3bbedba4a188842b922c8eb
Jeff Dembinski <jdembinski@gmail.com>
2018-05-12 16:39:45 -0400

    Partially Completed Assignment

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,27 @@
 module UsersHelper
+
+  def any_posts?(user)
+    if user.posts.count > 0
+      render user.posts
+    else
+      render html: tag.h4("#{user.name} has not created any posts yet.")
+    end
+  end
+
+  def any_comments?(user)
+    if user.comments.count > 0
+      render user.posts
+    else
+      render html: tag.h4("#{user.name} has not made any comments yet.")
+    end
+  end
+
+  def any_favorites?(user)
+    if user.favorites.count > 0
+      render user.favorites
+    else
+      render html: tag.h4("#{user.name} has not made any comments yet.")
+    end
+  end
+
 end
